0c6ca6809e79ba7274cf4111ae62dc6df4d912d1
Elisha Hollander <just4now666666@gmail.com>
2020-12-10 14:37:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'WikiQA',
-    license = open('LICENSE').read(),
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'A package to process wikipedia questions',
     long_description = open('README.md').read(),
     long_description_content_type = "text/markdown",
